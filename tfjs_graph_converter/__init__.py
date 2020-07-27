6779d9fd0f36a27c54dc50e48d2cfcb8ccc79205
patlevin <levin.patrick@gmail.com>
2020-07-28 00:26:14 +0200

    add public modules to package; fix #12

--- tfjs_graph_converter/__init__.py
+++ tfjs_graph_converter/__init__.py
@@ -1,6 +1,13 @@
 import os
 
-from tfjs_graph_converter import version
+# bring in all modules
+from tfjs_graph_converter import api                    # noqa: F401
+from tfjs_graph_converter import common as constants    # noqa: F401
+from tfjs_graph_converter import converter              # noqa: F401
+from tfjs_graph_converter import graph_rewrite_util     # noqa: F401
+from tfjs_graph_converter import optimization           # noqa: F401
+from tfjs_graph_converter import util                   # noqa: F401
+from tfjs_graph_converter import version                # noqa: F401
 
 # make tensorflow stop spamming messages
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
