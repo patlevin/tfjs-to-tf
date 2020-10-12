694ec9578487335ad489133c3860f8b63cc917c2
patlevin <levin.patrick@gmail.com>
2020-10-12 21:57:38 +0200

    suppress tensorflow log output

--- tfjs_graph_converter/__init__.py
+++ tfjs_graph_converter/__init__.py
@@ -1,5 +1,8 @@
 import os
 
+# make tensorflow stop spamming messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
+
 # bring in all modules
 from tfjs_graph_converter import api                    # noqa: F401
 from tfjs_graph_converter import common as constants    # noqa: F401
@@ -8,9 +11,6 @@ from tfjs_graph_converter import graph_rewrite_util     # noqa: F401
 from tfjs_graph_converter import optimization           # noqa: F401
 from tfjs_graph_converter import util                   # noqa: F401
 from tfjs_graph_converter import version                # noqa: F401
-
-# make tensorflow stop spamming messages
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
 
 __version__ = version.VERSION
 VERSION = version.VERSION
