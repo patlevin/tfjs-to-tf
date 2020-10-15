2ec57f4940517421f4aa882ba020b640cf5c53c0
patlevin <levin.patrick@gmail.com>
2020-10-16 01:43:22 +0200

    disable GPUs when while working with graphs

--- tfjs_graph_converter/__init__.py
+++ tfjs_graph_converter/__init__.py
@@ -2,15 +2,17 @@ import os
 
 # make tensorflow stop spamming messages
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
+# disable CUDA devices - we only want the CPU do work with data
+os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
 
 # bring in all modules
-from tfjs_graph_converter import api                    # noqa: F401
-from tfjs_graph_converter import common as constants    # noqa: F401
-from tfjs_graph_converter import converter              # noqa: F401
-from tfjs_graph_converter import graph_rewrite_util     # noqa: F401
-from tfjs_graph_converter import optimization           # noqa: F401
-from tfjs_graph_converter import util                   # noqa: F401
-from tfjs_graph_converter import version                # noqa: F401
+from tfjs_graph_converter import api                    # noqa: F401,E402
+from tfjs_graph_converter import common as constants    # noqa: F401,E402
+from tfjs_graph_converter import converter              # noqa: F401,E402
+from tfjs_graph_converter import graph_rewrite_util     # noqa: F401,E402
+from tfjs_graph_converter import optimization           # noqa: F401,E402
+from tfjs_graph_converter import util                   # noqa: F401,E402
+from tfjs_graph_converter import version                # noqa: F401,E402
 
 __version__ = version.VERSION
 VERSION = version.VERSION
