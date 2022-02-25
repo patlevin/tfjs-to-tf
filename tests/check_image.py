1a322e7182bb8275d9f170169d07b61e61d5e28a
patlevin <levin.patrick@gmail.com>
2022-02-25 21:43:00 +0100

    add CUDA support

--- tests/check_image.py
+++ tests/check_image.py
@@ -7,8 +7,10 @@ import sys
 import numpy as np
 import testutils as util
 import tensorflow as tf
+from typing import List
 from tensorflow.keras.layers.experimental.preprocessing import Resizing
 import tfjs_graph_converter as tfjs
+
 
 CLASS_NAME = ['horse', 'human']
 
@@ -22,31 +24,37 @@ def _evaluate(output):
     return (name, confidence)
 
 
-if len(sys.argv) != 2:
-    print(f"usage: {os.path.basename(sys.argv[0])} IMAGE")
-    exit(1)
+def main(args: List[str]) -> None:
+    if len(args) != 2:
+        print(f"usage: {os.path.basename(args[0])} IMAGE")
+        exit(1)
 
-if not os.path.isfile(sys.argv[1]):
-    print(f"{os.path.basename(sys.argv[0])}: '{sys.argv[1]}' is not a file")
-    exit(1)
+    if not os.path.isfile(args[1]):
+        print(f"{os.path.basename(args[0])}: '{args[1]}' isn't a file")
+        exit(1)
 
-image = tf.keras.preprocessing.image.load_img(sys.argv[1])
-image = tf.keras.preprocessing.image.img_to_array(image)
-# normalise image data to [-1, 1]
-image /= 127.5
-image -= 1.
-# ensure image size matches model input shape
-if image.shape != (32, 32, 3):
-    print(f"{os.path.basename(sys.argv[0])}: WARNING - image size "
-          f"should be 32x32, not {image.shape[0]}x{image.shape[1]}")
-    image = Resizing(height=32, width=32)(image)
-# reshape to fit model input (and convert to tensor if necessary)
-image = tf.reshape(image, [1, 32, 32, 3])
-# grab the model file and convert graph to function
-graph = util.get_sample_graph(util.get_path_to(util.DEPTHWISE_RELU_FILE))
-model = tfjs.api.graph_to_function_v2(graph)
-# run the model on the input image
-result = model(image)
-# show result
-label, confidence = _evaluate(result)
-print(f"Result: {label}, confidence={confidence}")
+    tfjs.api.enable_cuda()
+    image = tf.keras.preprocessing.image.load_img(args[1])
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    # normalise image data to [-1, 1]
+    image /= 127.5
+    image -= 1.
+    # ensure image size matches model input shape
+    if image.shape != (32, 32, 3):
+        print(f"{os.path.basename(args[0])}: WARNING - image size "
+              f"should be 32x32, not {image.shape[0]}x{image.shape[1]}")
+        image = Resizing(height=32, width=32)(image)
+    # reshape to fit model input (and convert to tensor if necessary)
+    image = tf.reshape(image, [1, 32, 32, 3])
+    # grab the model file and convert graph to function
+    graph = util.get_sample_graph(util.get_path_to(util.DEPTHWISE_RELU_FILE))
+    model = tfjs.api.graph_to_function_v2(graph)
+    # run the model on the input image
+    result = model(image)
+    # show result
+    label, confidence = _evaluate(result)
+    print(f"Result: {label}, confidence={confidence}")
+
+
+if __name__ == '__main__':
+    main(sys.argv)
